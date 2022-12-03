a44b2125ef4fe500f571817056ed5c544061683e
Jeromest <jerome.sf@outlook.com>
2022-12-04 00:25:56 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<el-card class="layout-home">
+		<p>
+			欢迎使用My Admin后台管理系统！
+		</p>
+	</el-card>
+</template>
+
+<style scoped>
+.layout-home {
+	line-height: 2;
+	font-size: 15px;
+}
+
+.layout-home h2 {
+	margin: 6px 0;
+}
+</style>
